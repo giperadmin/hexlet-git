df942a51ddfca47b79fa55271939075f945ef4d8
giperadmin <m26.s@bk.ru>
2025-11-09 19:14:06 +0300

    Математика в си

--- stepic_tests.c
+++ stepic_tests.c
@@ -1,30 +1,15 @@
 #include <stdio.h>
+#include <math.h>
 #include <windows.h>
-/*
-Телефонный номер
-Напишите программу, которая преобразует формат записи телефонного номера из такого +79111234567 в такой +7 (911) 123-45-67. В качестве разделителей используются пробелы.
-Номер телефона состоит из символа + и 11 произвольных цифр.
-Sample Input 1:
-+79111010203
-Sample Output 1:
-+7 (911) 101-02-03
-Sample Input 2:
-+12345678901
-Sample Output 2:
-+1 (234) 567-89-01
-*/
-
 int main(void)
 {
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
-    int rgn=0, prvd=0, n1=0,n2=0,n3=0;
-    printf("Введите номер телефона:\n");
-    scanf("%*c%1d%3d%3d%2d%2d",&rgn, &prvd, &n1,&n2,&n3);
-    //printf("+%1d (%03d) %03d-%02d-%02d", rgn, prvd, n1,n2,n3);
-    printf("%+1d (%.3d) %.3d-%.2d-%.2d", rgn, prvd, n1,n2,n3);
+    int x1=0, x2=0;
+    printf("Введите координаты\n");
+    scanf("%d%d", &x1, &x2);
+    printf("%d", abs(x2-x1));
     return 0;
 }
-//F9 to RUN in Code::Blocksb
+//F9 to RUN in Code::Blocks
 //F2 view logs window
-
