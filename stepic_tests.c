d0bae0499f60543765cba71ea545bc1d2bfa19e4
giperadmin <m26.s@bk.ru>
2025-11-09 08:45:32 +0300

    Scanf: задача про шахматную нотацию

--- stepic_tests.c
+++ stepic_tests.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
 #include <windows.h>
-
 int main(void)
 
 {
-        SetConsoleOutputCP(1251);
-        SetConsoleCP(1251);
-        int a = 0, b = 0, res;
-        printf("Введите два числа: \n");
-        scanf("%d", &a);
-        scanf("%d", &b);
-        res = a + b;
-        printf("%d + %d = %d\n", a, b, res);
-        return 0;
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    char  h1="_", h2="_", f ="_", sep="-";
+    int n=0, c1=0, c2=0, m=0;
+    printf("Введите полную нотацию\n");
+    scanf("%c", &f);
+    scanf(" %c", &h1);
+    scanf(" %d", &c1);
+    scanf(" %c", &sep);
+    scanf(" %c", &h2);
+    scanf(" %d", &c2);
+    //printf("%c,%c,%d,%c,%c,%d", f,h1,c1,sep,h2,c2);
+    printf("|%c%c%d|", f,h2,c2);
+    return 0;
 }
 //F9 to RUN in Code::Blocksb
+
+/*
+ПРАВИЛЬНОЕ РЕШЕНИЕ
+
+#include <stdio.h>
+int main(void)
+{
+    char figure;
+    char x; // координата A-H
+    int y; // координата 1-8
+
+    scanf("%c%*c%*d - %c%d", &figure, &x, &y);
+    printf("|%c%c%d|", figure, x, y);
+
+    return 0;
+}
+*/
