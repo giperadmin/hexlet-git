c51561c0a1731bc3ec3db89776f641ae85729e17
giperadmin <m26.s@bk.ru>
2025-11-10 21:29:39 +0300

    Генерация "случайных" чисел

--- stepic_tests.c
+++ stepic_tests.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
 #include <windows.h>
 
-//Формула Герона
+//Приоритет операций
 int main(void)
 {
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
-    double a=0,b=0,c=0,p=0;
-    scanf("%lf%lf%lf",&a,&b,&c);
-    p=0.5*(a+b+c);
-    printf("%.2f", sqrt(p*(p-a)*(p-b)*(p-c)));
+    srand(time(NULL));
+    printf("%d\n", 80 + rand()%(100 - 80 + 1));
+    printf("%d\n",rand()% 101);
+    printf("%d\n",rand()% 101);
+    printf("%d\n",rand()% 101);
+    printf("%d\n",rand()% 101);
+    printf("%d\n",rand()% 101);
+    printf("%d\n",rand()% 101);
+    printf("%d\n",rand()% 101);
+    printf("%d\n",rand()% 101);
+    printf("%d\n",rand()% 101);
+    printf("%d\n",rand()% 101);
     return 0;
 }
 //F9 to RUN in Code::Blocks
