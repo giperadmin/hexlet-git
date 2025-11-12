c7ad61df1598253a226894b353f6c92dc7db8225
giperadmin <m26.s@bk.ru>
2025-11-12 20:52:34 +0300

    Формула Тейлора

--- stepic_tests.c
+++ stepic_tests.c
@@ -4,44 +4,32 @@
 #include <time.h>
 #include <windows.h>
 
-//„исло Ёйлера
+//e^x
 int main(void)
 {
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
-    int x=0;
-    double res=1, e =1;
-    res = res * ++x;
-    e=e+1/res;
-    res = res * ++x;
-    e=e+1/res;
-    res = res * ++x;
-    e=e+1/res;
-    printf("%.5f\n",e);
 
-    res=1; e=1; x=0;
-    res = res * ++x;
-    e=e+1/res;
-    res = res * ++x;
-    e=e+1/res;
-    res = res * ++x;
-    e=e+1/res;
-    res = res * ++x;
-    e=e+1/res;
-    printf("%.5f\n",e);
+    int s=1;
+    double res=1, n =1, x=1, ex=1,f=1, ch=1;
+    printf("¬ведите степень\n");
+    scanf("%d",&s);
+    x=s;
+    ch=ch*x;f=f*n; ex=ex+ch/f; n=n+1;
+    ch=ch*x;f=f*n; ex=ex+ch/f; n=n+1;
+    ch=ch*x;f=f*n; ex=ex+ch/f; n=n+1;
+    ch=ch*x;f=f*n; ex=ex+ch/f; n=n+1;
+    ch=ch*x;f=f*n; ex=ex+ch/f; n=n+1;
 
-    res=1; e=1; x=0;
-    res = res * ++x;
-    e=e+1/res;
-    res = res * ++x;
-    e=e+1/res;
-    res = res * ++x;
-    e=e+1/res;
-    res = res * ++x;
-    e=e+1/res;
-    res = res * ++x;
-    e=e+1/res;
-    printf("%.5f\n",e);
+    ch=ch*x;f=f*n; ex=ex+ch/f; n=n+1;ch=ch*x;f=f*n; ex=ex+ch/f; n=n+1;ch=ch*x;f=f*n; ex=ex+ch/f; n=n+1;ch=ch*x;f=f*n; ex=ex+ch/f; n=n+1;ch=ch*x;f=f*n; ex=ex+ch/f; n=n+1;ch=ch*x;f=f*n; ex=ex+ch/f; n=n+1;ch=ch*x;f=f*n; ex=ex+ch/f; n=n+1;
+
+
+
+    printf("%.6f\n",ex);
+    printf("%.6f\n",exp(s));
+    printf("%f\n",exp(s)-ex);
+
+
     //system("pause"); //помогает от некоторых ошибок
     return 0;
 }
