ff6776e5ec9148e7082d41bc3f6f495f92fa69e5
giperadmin <m26.s@bk.ru>
2025-11-16 20:28:56 +0300

    Квадранты

--- stepic_tests.c
+++ stepic_tests.c
@@ -5,7 +5,7 @@
 #include <windows.h>
 #include <locale.h>
 
-//switch Конвертер прав доступа UNIX
+//
 
 int main(void)
 {
@@ -13,28 +13,19 @@ int main(void)
     SetConsoleCP(1251);
     setlocale(LC_ALL, "");
 
-    char c1,c2,c3;
-    int n=0;
-    scanf("%1c%1c%1c",&c1,&c2,&c3);
-    switch (c1)
-    {
-        case 'r': n=n+4; break;
-        case '-': n=n+0; break;
-        default: printf("Invalid format"); return 0;
-    }
-    switch (c2)
-    {
-        case 'w': n=n+2; break;
-        case '-': n=n+0; break;
-        default: printf("Invalid format"); return 0;
-    }
-    switch (c3)
-    {
-        case 'x': n=n+1; break;
-        case '-': n=n+0; break;
-        default: printf("Invalid format"); return 0;
-    }
-    printf("%d",n);
+    double x=1, y=1;
+    int k=1;
+    //scanf("%lf%lf", &x,&y);
+    x=15.24;
+    y=-21.121;
+    k=  ((x>0)&&(y>0))*1+
+        ((x<0)&&(y>0))*2+
+        ((x<0)&&(y<0))*3+
+        ((x>0)&&(y<0))*4;
+
+    printf("%d\n",k);
+
+
     //system("pause"); //помогает от некоторых ошибок
     return 0;
 }
