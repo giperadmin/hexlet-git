24198d1fe5a1b81c5bc102cc4d33747f22820dab
giperadmin <m26.s@bk.ru>
2025-11-16 15:43:59 +0300

    расчёт BMR

--- stepic_tests.c
+++ stepic_tests.c
@@ -11,13 +11,36 @@ int main(void)
 {
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
-    int k=5;
-    printf("%d\n",k%10);
+    setlocale(LC_ALL, "");
+
+    char sex='m';
+    int age, height, weight;
+    double bmr_male, bmr_female;
+
+        scanf("%c%d%d%d",&sex,&age,&height,&weight);
+
+        switch (sex)
+        {
+            case 'm':
+                printf("|  BMR  |\n");
+                bmr_male = 10*weight + 6.25*height - 5*age + 5;
+                printf("|%7.2f|\n", bmr_male);
+                break;
+        case 'f':
+                printf("|  BMR  |\n");
+                bmr_female = 10*weight + 6.25*height - 5*age - 161;
+                printf("|%7.2f|\n", bmr_female);
+                break;
+        default:
+            printf("ERROR!\n");
+            break;
+
+        }
 
 
 
 
-    system("pause"); //помогает от некоторых ошибок
+    //system("pause"); //помогает от некоторых ошибок
     return 0;
 }
 //F9 to RUN in Code::Blocks
