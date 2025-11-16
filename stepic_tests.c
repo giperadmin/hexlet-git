191a3dd12b83f87d98255140197055cc6ca7e70c
giperadmin <m26.s@bk.ru>
2025-11-16 09:03:00 +0300

    §6.1 switch

--- stepic_tests.c
+++ stepic_tests.c
@@ -3,21 +3,46 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
+#include <locale.h>
 
-//„Єт-нечет (1 или -1)
+//switch
 
 int main(void)
 {
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
+ char answer;
 
-    int  k=0, e=0, isEvent=0;
-    double a=0;
-    scanf("%d",&k);
-    isEvent = k%2;
-    e=isEvent-(2*(isEvent));
-    a = pow(e,isEvent);
-    printf("%.0f\n",a);
+        setlocale(LC_ALL, ""); // чтобы использовать русские символы
+
+        printf("¬ыберите правильный вариант ответа.\n\n");
+        printf("Ќачало и конец ¬торой мировой войны:\n");
+        printf("\ta. 1 сент€бр€ 1939 Ч 9 ма€ 1945\n");
+        printf("\tb. 1 сент€бр€ 1939 Ч 2 сент€бр€ 1945\n");
+        printf("\tc. 22 июн€ 1941 Ч 9 ма€ 1945\n");
+        printf("\td. 22 июн€ 1941 Ч 2 сент€бр€ 1945\n");
+
+        printf("\n¬ведите ваш вариант ответа: ");
+        scanf("%c", &answer);
+
+        switch (answer) {
+                case 'a':
+                        printf("BAD!\n");
+                        break;
+                case 'b':
+                        printf("GOOD!\n");
+                        break;
+                case 'c':
+                        printf("BAD!\n");
+                        break;
+                case 'd':
+                        printf("BAD!\n");
+                        break;
+                default:
+                        printf("ERROR!\n");
+                        break;
+        }
+
 
     system("pause"); //помогает от некоторых ошибок
     return 0;
