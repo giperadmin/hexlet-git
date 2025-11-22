8b70168979d4059f1ae4328bd8670c7748b233b5
giperadmin <m26.s@bk.ru>
2025-11-22 18:24:42 +0300

    Фиббоначи

--- stepic_tests.c
+++ stepic_tests.c
@@ -5,7 +5,7 @@
 #include <windows.h>
 #include <locale.h>
 
-//Радар
+//Фиббоначи
 
 int main(void)
 {
@@ -13,14 +13,12 @@ int main(void)
     SetConsoleCP(1251);
     setlocale(LC_ALL, "");
 
-    int x1,y1,x2,y2;
-    x1=y1=x2=y2=0;
-    scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
-    if(x1*x1+y1*y1<x2*x2+y2*y2){printf("1\n");}
-    else if(x1*x1+y1*y1>x2*x2+y2*y2){printf("2\n");}
-    else{printf("0\n");}
+    int i=0,n=0,count=1;
+    scanf("%d",&n);
+    for(i=2; i<=n/2; i++)
+        if (n%i==0){count=0;}
+    printf("%d",count);
     //system("pause"); //помогает от некоторых ошибок
-
     return 0;
 }
 //F9 to RUN in Code::Blocks
