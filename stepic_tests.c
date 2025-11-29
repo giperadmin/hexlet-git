85c9549d6d9124c06785c8bdde32e891beee6a12
giperadmin <m26.s@bk.ru>
2025-11-29 12:59:15 +0300

    Циклическая перестановка

--- stepic_tests.c
+++ stepic_tests.c
@@ -13,28 +13,55 @@ int main(void)
     SetConsoleCP(1251);
     setlocale(LC_ALL, "");
 
-    int N=1, max=0, min=0, imax=0,imin=0, x=0, i=0;
-    double M=0;
-    scanf("%d",&N); printf("\n");
-    int arr[N];
-    for(i=0; i<N; i++) scanf("%d",&arr[i]);
-    min=max=arr[0];
-    for(i=0; i<N; i++)
-                {if (arr[i]<=min) {min=arr[i]; imin=i;}
-                if (arr[i]>=max) {max=arr[i]; imax=i;}}
+    int i=0, j=0, N=4,M=4, s=0, col=0,row=0,c_min=0, c_max=0, min=0, max=0;
+    int k=5,delta=0, n=1, m=1;
+    int arr[4][4]=  {   {1,2,3,4},
+                        {1,2,3,4},
+                        {1,2,3,4},
+                        {1,2,3,4}    };
+    //scanf("%d",&N);
+    M=N;
+    /* int arr[N][M];
+    for(i=0;i<N;i++){
+        for(j=0;j<M;j++){scanf("%d",&arr[i][j]);}}
+    scanf("%d",&k);*/
 
-    printf("\n\nmin=%d max=%d\n\n",min,max);
-    for (i=0;i<N;i++) {
-            x=arr[i];
-            if (x==max) printf("%d ",min);
-            else if (x==min) printf("%d ",max);
-            else printf("%d ",x);
-    }
+    printf("=============================================\n");
+    for(i=0;i<N;i++){
+        for(j=0;j<M;j++){printf("%5d",arr[i][j]);}
+            printf("\n");}
+    printf("=============================================\n");
+    printf("K = %d, N = %d, M = %d\n",k,N,M);
+
+    if (k>M) {
+            k=k%M;
+            printf("optimized k = %d\n",k);}
+
+    int arr2[N][M];
+    i=j=0;
+    for(i=0;i<N;i++){
+        for(j=0;j<M;j++){
+                n=i;
+                if (j+k<=M) delta = 0;
+                if (j+k>M-1) delta = -M;
+
+                m=j+k+delta;
+                arr2[n][m]=arr[i][j];
+                printf(" i%dj%dn%dm%dd%d",i,j,n,m,delta);
+
+        } printf("\n");printf("\n");printf("\n");
+        }
 
 
 
+    printf("=============================================\n");
+    for(i=0;i<N;i++){
+        for(j=0;j<N;j++){
+                printf("%d ",arr2[i][j]);
+        }
+            printf("\n");}
+    printf("=============================================\n");
 
-    //system("pause"); //помогает от некоторых ошибок
     return 0;
 }
 //F9 to RUN in Code::Blocks
